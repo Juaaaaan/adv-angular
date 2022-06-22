642cdc9e9a2621d198aeab567b0fa76a43a5a4fb
Juan José Suárez <suarezramirezjuanjose@hotmail.com>
2022-06-22 16:53:07 +0200

    Develop (#1) (#3)
    * feat. replace all default angular content in angular template
    
    * feat add finally layout and app-routing.module
    
    * add router outlet
    
    * diseño listo

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
 import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
